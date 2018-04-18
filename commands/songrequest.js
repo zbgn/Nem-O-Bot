32e1d06a613af14e9f81cf98af13c5460c9945f4
Mystere98 <38164068+Mystere98@users.noreply.github.com>
2018-04-18 23:07:32 +0200

    anti HS / priority

--- commands/songrequest.js
+++ commands/songrequest.js
@@ -3,7 +3,14 @@ const songs = require('../songs')
 module.exports = {
   main: (bot, msg, settings) => {
     var content = msg.content.split(',')
-    songs.requested(msg.author.username, content[0].trim(), content[1].trim())
+    if (content.length === 2) {
+      isAdded = songs.requested(msg.author.username, content[0].trim(), content[1].trim())
+    }
+    if (typeof isAdded !== 'undefined' && isAdded) {
+      bot.sendNotification('Added', 'info', msg)
+    } else {
+      bot.sendNotification('Error', 'error', msg)
+    }
   },
   args: `'<string>' '<string>'`,
   help: 'Send a song to the song request list',
