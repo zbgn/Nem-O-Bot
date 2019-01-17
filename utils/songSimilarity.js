6acb95c735311c2e85117472d005974135f563c2
Gysco <zackary.beaugelin@epitech.eu>
2019-01-17 17:27:07 +0100

    update to match 'artist - song' & 'song - artist'

--- utils/songSimilarity.js
+++ utils/songSimilarity.js
@@ -6,6 +6,12 @@ module.exports = {
   getSong: (song, artist, next) => {
     var songlist = JSON.parse(fs.readFileSync('./utils/musicstream.json', 'utf-8'))
     var author = ss.findBestMatch(String(artist), Object.keys(songlist)).bestMatch
+    if (author.rating < 0.8) {
+      var swap = song
+      song = artist
+      artist = swap
+    }
+    author = ss.findBestMatch(String(artist), Object.keys(songlist)).bestMatch
     if (author.rating >= 0.8) {
         var music = ss.findBestMatch(String(song), songlist[author.target]).bestMatch
         if (music.rating >= 0.8) next(author.target, music.target)
